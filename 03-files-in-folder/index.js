26a5b56da6331af4e9b63dc7633628452d4fdc13
egorokunevich <mrcraigre@gmail.com>
2024-01-20 01:04:21 +0300

    refactor: match output format to requirements

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const secretPath = path.join(__dirname, 'secret-folder');
+
 fs.readdir(secretPath, (err, files) => {
   err ? console.log(err) : null;
   files.forEach((file) => {
     const currentPath = path.join(secretPath, file);
+
     fs.stat(currentPath, (err, stats) => {
       err ? console.log(err) : null;
+
       if (!stats.isDirectory()) {
+        const fileBaseName = path.parse(currentPath, file).name;
+        const fileExtension = path.extname(currentPath, file);
+        const fileSize = (stats.size / 1024).toFixed(2);
+
         process.stdout.write(
-          `File: ${path
-            .parse(currentPath, file)
-            .name.padEnd(6)} | extension: ${path
-            .extname(currentPath, file)
-            .padEnd(5)} | size: ${Math.ceil(stats.size / 1024)} KB\n`,
+          `${fileBaseName} - ${fileExtension} - ${fileSize} KB\n`,
         );
       }
     });
