77e7289a59fa71d5b9926c7ee895231a5759a563
egorokunevich <mrcraigre@gmail.com>
2024-01-23 16:56:03 +0300

    feat: implement farewell message and exit command

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,11 +2,21 @@ const { stdin, stdout } = process;
 const fs = require('fs');
 const path = require('path');
 fs.writeFile(path.join(__dirname, 'text.txt'), '', () => {
-  console.log('Created text.txt');
   stdout.write('Please, enter the data...\n');
 });
 stdin.on('data', (data) => {
-  fs.appendFile(path.join(__dirname, 'text.txt'), data, () => {
-    stdout.write('Please, enter the data... (or Ctrl+C to exit)\n');
-  });
+  if (data.toString().trim() === 'exit') {
+    stdin.destroy();
+  } else {
+    fs.appendFile(path.join(__dirname, 'text.txt'), data, () => {
+      stdout.write(`Please, enter the data... (Ctrl+C or input 'exit' to stop the process)\n`);
+    });
+  }
+});
+stdin.on('close', () => {
+  console.log('Process stopped.');
+});
+process.on('SIGINT', () => {
+  console.log('Process stopped.');
+  process.exit();
 });
